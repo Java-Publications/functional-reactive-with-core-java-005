f66a79cdce3fbba66465674c57d9360d253f58dc
Sven Ruppert <sven.ruppert@gmail.com>
2017-08-17 20:49:53 +1000

    v004

---
+++ src/main/java/org/rapidpm/frp/v004/Main.java
@@ -0,0 +1,67 @@
+package org.rapidpm.frp.v004;
+
+import java.util.function.Function;
+
+import org.rapidpm.frp.model.Result;
+
+/**
+ *
+ */
+public class Main {
+
+  public static interface Service {
+    String doWork(String txt) throws Exception;
+  }
+
+  @FunctionalInterface
+  public interface CheckedFunction<T, R> extends Function<T, Result<R>> {
+    @Override
+    default Result<R> apply(T t) {
+      try {
+        return Result.success(applyWithException(t));
+      } catch (Exception e) {
+        final String message = e.getMessage();
+        return Result.failure((message != null) ? message : e.getClass().getSimpleName());
+      }
+    }
+
+    R applyWithException(T t) throws Exception;
+
+  }
+
+  public static void main(String[] args) {
+
+
+    final Function<String, Result<String>> checkedFunction
+        = (CheckedFunction<String, String>)((Service) txt -> txt.toUpperCase() + "-workedOn")::doWork;
+
+
+    checkedFunction.apply("Hello")
+                   .ifPresentOrElse(
+                       (result) -> System.out.println("result = " + result),
+                       (failed) -> System.out.println("failed = " + failed)
+                   );
+
+    checkedFunction.apply("Hello")
+                   .ifPresentOrElse(
+                       (result) -> System.out.println("result = " + result),
+                       () -> System.out.println("failed = execute something like logging ?")
+                   );
+
+    checkedFunction.apply(null)
+                   .ifPresentOrElse(
+                       (result) -> System.out.println("result = " + result) ,
+                       (failed) -> System.out.println("failed = " + failed)
+                   );
+
+    checkedFunction.apply(null)
+                   .ifPresentOrElse(
+                       (result) -> System.out.println("result = " + result) ,
+                       () -> System.out.println("failed = execute something like logging ?")
+                   );
+
+
+
+
+  }
+}
