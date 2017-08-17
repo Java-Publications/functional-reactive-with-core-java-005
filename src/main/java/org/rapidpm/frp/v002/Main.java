4933a3dedb6e4611e2789aa45fc0f860b5eb8cc6
Sven Ruppert <sven.ruppert@gmail.com>
2017-08-17 20:49:30 +1000

    v002

---
+++ src/main/java/org/rapidpm/frp/v002/Main.java
@@ -0,0 +1,30 @@
+package org.rapidpm.frp.v002;
+
+import java.util.Optional;
+
+/**
+ *
+ */
+public class Main {
+
+  public static interface Service {
+    String doWork(String txt) throws Exception;
+  }
+
+
+  public static void main(String[] args) {
+
+
+    Optional<String> optional;
+    try {
+      final String result = ((Service) txt -> txt.toUpperCase() + "-workedOn").doWork("");
+      optional = Optional.of(result);
+    } catch (Exception e) {
+      e.printStackTrace();
+      optional = Optional.empty();
+    }
+
+    optional.ifPresent((result)-> System.out.println("result = " + result));
+
+  }
+}
