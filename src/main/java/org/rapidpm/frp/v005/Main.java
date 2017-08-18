52aa34eeb5341953efc72d25ff5f03583b3f6168
Sven Ruppert <sven.ruppert@gmail.com>
2017-08-18 10:07:39 +1000

    v005

---
+++ src/main/java/org/rapidpm/frp/v005/Main.java
@@ -0,0 +1,40 @@
+package org.rapidpm.frp.v005;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntConsumer;
+import java.util.stream.Stream;
+
+import org.rapidpm.frp.functions.CheckedFunction;
+import org.rapidpm.frp.model.Result;
+
+/**
+ *
+ */
+public class Main {
+
+  public static interface Service {
+    String doWork(String txt) throws Exception;
+  }
+
+  private static IntConsumer print = System.out::println;
+
+  public static void main(String[] args) {
+    //from now on using functional-reactive lib
+    //https://github.com/functional-reactive/functional-reactive-lib
+
+    final Function<String, Result<Integer>> f = (CheckedFunction<String, Integer>) Integer::valueOf;
+
+    Stream
+        .of("1" , "2" , "Hi" , "3")
+        .parallel()
+        .map(f)
+        .filter(Result::isPresent)
+        .mapToInt(Result::get)
+        .reduce((left , right) -> left + right)
+        .ifPresent(print);
+
+
+  }
+}
